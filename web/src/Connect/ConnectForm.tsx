14b75d9ace0b37734f99de512c9231c5beb7875c
Emilia Dreamer <emilia@rymiel.space>
2021-11-23 21:25:18 +0200

    Allow pressing enter for host-port connect

--- web/src/Connect/ConnectForm.tsx
+++ web/src/Connect/ConnectForm.tsx
@@ -17,6 +17,7 @@ export default class ConnectForm extends React.Component<ConnectFormProps, Conne
     this.state = {port: 25565};
     this.handleHostChange = this.handleHostChange.bind(this);
     this.handlePortChange = this.handlePortChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleHostChange(ev: React.ChangeEvent<HTMLInputElement>) {
@@ -27,10 +28,18 @@ export default class ConnectForm extends React.Component<ConnectFormProps, Conne
     this.setState({port: parseInt(ev.currentTarget.value)});
   }
 
+  handleSubmit(ev: React.KeyboardEvent<HTMLInputElement>) {
+    if (ev.key === 'Enter') {
+      ev.preventDefault();
+      if (this.state.host !== undefined && this.state.port !== undefined)
+        this.props.app.connect(this.state.host, this.state.port);
+    }
+  }
+
   render() {
     return <div style={{justifyContent: "center", display: "flex", marginBottom: "1em"}}>
-      <input placeholder="play.vanillarite.com" onChange={this.handleHostChange} />
-      <input type="number" defaultValue="25565" size={5} onChange={this.handlePortChange} />
+      <input placeholder="play.vanillarite.com" onKeyDown={this.handleSubmit} onChange={this.handleHostChange} />
+      <input type="number" defaultValue="25565" size={5} onKeyDown={this.handleSubmit} onChange={this.handlePortChange} />
       <ConnectButton app={this.props.app} host={[this.state.host, this.state.port]} />
     </div>
   }
