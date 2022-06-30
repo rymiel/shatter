f11bf9607e6de34c9bb6f797c63ab2cea5fa81f3
Emilia Dreamer <emilia@rymiel.space>
2022-06-30 20:06:18 +0300

    Improve console "client" startup

--- src/console.cr
+++ src/console.cr
@@ -1,17 +1,23 @@
 require "./shatter"
 
-puts "1/6: MSA"
+puts "1/8: MSA"
 msa = Shatter::MSA.new
-puts "2/6: Token"
-token = msa.refresh ARGV[2]
-puts "3/6: XBL"
+puts "2/8: Token"
+token = msa.refresh ARGV[3]
+puts "3/8: XBL"
 xbl = msa.xbl token
-puts "4/6: XSTS"
+puts "4/8: XSTS"
 xsts = msa.xsts xbl
-puts "5/6: Minecraft"
+puts "5/8: Minecraft"
 mc_token = msa.minecraft xsts
-puts "6/6: Checking profile"
+puts "6/8: Checking profile"
 profile = msa.profile mc_token
+
+puts "7/8: Priming registry"
 registry, known_blocks = Shatter.local_registry
-Shatter::Connection.new(Shatter::Packet::Protocol::PROTOCOL_NAMES[ARGV[0]], ARGV[1], ARGV[2].to_i, registry, known_blocks, mc_token.access_token, profile).ping
+puts "8/8: Priming translation keys"
+Shatter::Chat::Reader::MojangAssetLangReader.new.keys
+
+puts "Done! Connecting..."
+Shatter::Connection.new(Shatter::Packet::Protocol::PROTOCOL_NAMES[ARGV[0]], ARGV[1], ARGV[2].to_i, registry, known_blocks, mc_token.access_token, profile).run
 sleep
